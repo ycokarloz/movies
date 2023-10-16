a0099b830e63b46c3588f4ee8b132cfaf29b5773
Ubuntu <ubuntu@ip-172-31-5-110.ap-southeast-2.compute.internal>
2023-10-16 22:53:39 +0000

    20231017 update

--- src/main/java/dev/seven/movies/Movie.java
+++ src/main/java/dev/seven/movies/Movie.java
@@ -3,10 +3,20 @@ package dev.seven.movies;
 import java.util.List;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
+    @Id 
     private ObjectId id;
     private String imdbId;
     private String title;
@@ -15,4 +25,7 @@ public class Movie {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+
+    @DocumentReference
+    private List<Review> reviewIds;
 }
